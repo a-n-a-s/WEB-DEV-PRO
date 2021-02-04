be2034237fbe449a8ab1e4962ad4e4261c0fa0d8
a-n-a-s <68739372+a-n-a-s@users.noreply.github.com>
2021-02-04 16:32:02 +0530

    Add files via upload

---
+++ CoronoMap/index.js
@@ -0,0 +1,27 @@
+function updateMap() {
+  fetch("/data.json")
+    .then((response) => response.json())
+    .then((rsp) => {
+      console.log(rsp.data);
+      rsp.data.forEach((element) => {
+        latitude = element.latitude;
+        longitude = element.longitude;
+
+        cases = element.infected;
+        if(cases>225){
+            color = "rgb(225 , 0 , 0)";
+        }
+        else{
+            color = `"rgba(${cases} , 0 , 0)"`;
+        }
+
+        new mapboxgl.Marker({
+          draggable: false,
+          color : color
+        })
+          .setLngLat([longitude, latitude])
+          .addTo(map);
+      });
+    });
+}
+updateMap();
